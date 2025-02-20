39f67189aa2898d5cae42f7d558fa79b34c900db
xingjun.wxj <xingjun.wxj@alibaba-inc.com>
2025-02-21 01:02:26 +0800

    update run_grpo_rec.sh

--- src/open-r1-multimodal/run_grpo_rec.sh
+++ src/open-r1-multimodal/run_grpo_rec.sh
@@ -6,7 +6,17 @@ export DEBUG_MODE="true"
 RUN_NAME="Qwen2.5-VL-3B-GRPO-REC"
 export LOG_PATH="./debug_log_$RUN_NAME.txt"
 
-torchrun --nproc_per_node="8" \
+
+## Modified from the original script, origin:
+# --nproc_per_node="8"
+# --model_name_or_path Qwen/Qwen2.5-VL-3B-Instruct
+# --image_root <your_image_root>
+
+## Examples
+# data: train2014/COCO_train2014_000000283267.jpg
+
+
+torchrun --nproc_per_node="4" \
     --nnodes="1" \
     --node_rank="0" \
     --master_addr="127.0.0.1" \
@@ -14,9 +24,9 @@ torchrun --nproc_per_node="8" \
     src/open_r1/grpo_rec.py \
     --deepspeed local_scripts/zero3.json \
     --output_dir output/$RUN_NAME \
-    --model_name_or_path Qwen/Qwen2.5-VL-3B-Instruct \
+    --model_name_or_path /mnt/workspace/xingjun.wxj/vlm_r1_work/models/Qwen2.5-VL-3B-Instruct \
     --dataset_name data_config/rec.yaml \
-    --image_root <your_image_root> \
+    --image_root /mnt/workspace/xingjun.wxj/vlm_r1_work/VLM-R1/data/train2014 \
     --max_prompt_length 1024 \
     --num_generations 8 \
     --per_device_train_batch_size 1 \
@@ -28,7 +38,7 @@ torchrun --nproc_per_node="8" \
     --report_to wandb \
     --gradient_checkpointing false \
     --attn_implementation flash_attention_2 \
-    --num_train_epochs 2 \
+    --num_train_epochs 1 \
     --run_name $RUN_NAME \
     --save_steps 100 \
     --save_only_model true
