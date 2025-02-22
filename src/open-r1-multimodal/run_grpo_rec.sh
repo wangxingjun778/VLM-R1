bcb50df7cf7b05d80a47ffb600dc6a69782471d8
xingjun.wxj <xingjun.wxj@alibaba-inc.com>
2025-02-22 22:31:04 +0800

    update

--- src/open-r1-multimodal/run_grpo_rec.sh
+++ src/open-r1-multimodal/run_grpo_rec.sh
@@ -3,7 +3,7 @@ cd src/open-r1-multimodal
 export DEBUG_MODE="true"
 # export CUDA_VISIBLE_DEVICES=4,5,6,7
 
-RUN_NAME="Qwen2.5-VL-3B-GRPO-REC"
+RUN_NAME="Qwen2.5-VL-7B-GRPO-REC"
 export LOG_PATH="./debug_log_$RUN_NAME.txt"
 
 
@@ -32,7 +32,7 @@ torchrun --nproc_per_node="4" \
     src/open_r1/grpo_rec.py \
     --deepspeed local_scripts/zero3.json \
     --output_dir output/$RUN_NAME \
-    --model_name_or_path /mnt/workspace/xingjun.wxj/vlm_r1_work/models/Qwen2.5-VL-3B-Instruct \
+    --model_name_or_path /mnt/workspace/xingjun.wxj/vlm_r1_work/models/Qwen2.5-VL-7B-Instruct \
     --dataset_name data_config/rec.yaml \
     --image_root /mnt/workspace/xingjun.wxj/vlm_r1_work/VLM-R1/data \
     --max_prompt_length 1024 \
@@ -49,6 +49,6 @@ torchrun --nproc_per_node="4" \
     --num_train_epochs 2 \
     --run_name $RUN_NAME \
     --save_steps 100 \
-    --save_only_model true \
-    --resume_from_checkpoint /mnt/workspace/xingjun.wxj/vlm_r1_work/VLM-R1/src/open-r1-multimodal/output/Qwen2.5-VL-3B-GRPO-REC/checkpoint-2900
+    --save_only_model true
+#    --resume_from_checkpoint /mnt/workspace/xingjun.wxj/vlm_r1_work/VLM-R1/src/open-r1-multimodal/output/Qwen2.5-VL-3B-GRPO-REC/checkpoint-2900
 
